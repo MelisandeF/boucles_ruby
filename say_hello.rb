ffe2aff094609fb1e366eb88d0a8246aca3a78bc
MelisandeF <fleisch.melisande@gmail.com>
2018-10-02 11:15:10 +0200

    add 1.2 & 1.3

--- say_hello.rb
+++ say_hello.rb
@@ -1,3 +1,15 @@
+#Dis Bonjour
+
+def bonjour
+
+	return "Bonjour"
+
+end
+
+puts bonjour
+
+#first_name
+
 def bonjour(nom)
 
 	return "Bonjour #{nom}"
@@ -5,3 +17,15 @@ def bonjour(nom)
 end
 
 puts bonjour('Sofia')
+
+#Le combo
+
+puts "Hello what's your name?"
+name = gets.chomp
+
+def bonjour 
+	return "Bonjour #{name}"
+
+end
+
+puts "Bonjour #{name}"
