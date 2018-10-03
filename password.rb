bc40f15942141ae082e5c6030f5675f0556df8de
MelisandeF <fleisch.melisande@gmail.com>
2018-10-03 14:49:22 +0200

    Ca y est c'est enfin géré !

--- password.rb
+++ password.rb
@@ -13,11 +13,33 @@ puts "Merci de tapper le mot de passe une seconde fois"
 def password_verification(second_password = gets.chomp)
 	@second_password = second_password
 	if @user_password != @second_password
-		puts "Le mot de passe ne correspond pas au premier"
+		puts "Le mot de passe ne correspond pas au premier, tente à nouveau ta chance"
+		return password_verification(second_password = gets.chomp)
 	elsif puts "Merci"
 	end			
 
 end
 
 password_verification
+
+
+#Se connecter avec le mot de passe
+
+puts "Ton mot de passe a bien été enregistré"
+puts "Tu peux maintenant accéder à 'Rubyconnect'"
+puts "Pour te connecter à la plateforme 'Rubyconnect' entre ton mot de passe :"
+
+
+def connect(password = gets.chomp)
+	@password = password
+
+	if @password != @user_password 
+		puts "Hé mon loulou c'est faux, tente à nouveau ta chance"
+		return connect(password = gets.chomp)
+	else puts "Bienvenue sur Rubyconnect"
+	end
+end
+
+
+connect 
 
