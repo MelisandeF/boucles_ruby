de74a84531a39a496856dfe828d5c5193198c078
MelisandeF <fleisch.melisande@gmail.com>
2018-10-04 14:07:37 +0200

    excercice in progress

--- mario.rb
+++ mario.rb
@@ -4,32 +4,32 @@ puts " Si tu fais 1, tu descend d'une case"
 puts " Si tu fais 2, 3 ou 4 rien ne se passe"
 puts " Si tu fais 5 ou 6 tu avances d'une marche"
 @position = 1 
-#def lancé
+
 while @position != 10 
 puts nb = (rand(6)+1) 
 
-#def one(nb)
+def one(nb)
 if (nb  == 1)
     puts "Tu descend d'une case"
     puts "#{@position -1}"
   
     @position = @position -1
-    #return
-#end
+   
+end
 end
 
 
-#def two(nb)
+def two(nb)
 if (nb  == 2) || (nb  == 3) || (nb == 4)
     puts "Pas bougé ! "
     puts "#{@position}"
    
     @position = @position
-#end
+end
 end
 
 
-#def six(nb)
+def six(nb)
 if (nb  == 5) || (nb == 6)
     
     puts "Bravo! "
@@ -37,13 +37,13 @@ if (nb  == 5) || (nb == 6)
    
     @position = @position +1
    
-    #return
-#end
+   
+end
 
 end
-#puts one(nb)
-#puts two(nb)
-#puts six(nb)
+puts one(nb)
+puts two(nb)
+puts six(nb)
 
 puts ""
 end
